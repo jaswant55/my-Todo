aeedf68da8adb51884e88d6170f304c2c5f7ad17
jaswant55 <jaswantpatel2000jassu@gmail.com>
2022-04-13 16:37:56 +0530

    update scr files

---
+++ src/componets/Todos.js
@@ -0,0 +1,26 @@
+ import React,{useContext} from "react";
+ import { ListGroup, ListGroupItem } from "reactstrap";
+ import {FaCheckDouble} from "react-icons/fa";
+ import { ToDoContext } from "../context/ToDoContext";
+ import { REMOVE_TODO } from "../context/action.types";
+
+ const Todos=()=>{
+         const [todos, dispatch]= useContext(ToDoContext)
+         return(
+            <ListGroup className="mt-5 mb-2 items">
+             {todos.map(todo=>(
+                <ListGroupItem key={todo.id}>{todo.todoString}
+                 <span><FaCheckDouble/></span>
+                    
+
+                 </ListGroupItem>
+  
+             ))}
+             
+
+             </ListGroup>
+         )
+ };
+
+
+ export default Todos;
